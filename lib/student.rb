72bf3cbdb2516e7cf32bcf6f5c1c7ecdf6c143cf
jay <jaypifiverr@gmail.com>
2022-12-25 03:45:17 -0500

    commit tree 33deeb492a8a2a289d6fdafaab826a6d8748673f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative './user'
+
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(info)
+        return @knowledge << info
+    end
+
+    def knowledge
+        return @knowledge
+    end
 
 end
+
+fido = Student.new
+fido.learn('heyyy')
+fido.knowledge
